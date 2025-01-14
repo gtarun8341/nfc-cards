be5879274ba580180b74649449ae690856727414
gtarun8341 <gtarun8341@gmail.com>
2025-01-14 12:35:33 +0530

    domain name changedcommit

--- src/app/apiConfig/axiosConfig.js
+++ src/app/apiConfig/axiosConfig.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 // Create an Axios instance with the base URL
 const api = axios.create({
   // baseURL: 'http://localhost:5000', // Replace this with your actual base URL
-  baseURL: 'https://api.shivencardds.com', // Replace this with your actual base URL
+  baseURL: 'https://api.shivenfccaards.com', // Replace this with your actual base URL
 
 });
 
